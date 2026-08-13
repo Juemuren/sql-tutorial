8fe00a748c5a7de807219f8e8c055fa06c84a058
Raind <127611616+Juemuren@users.noreply.github.com>
2026-08-13 11:34:15 +0800

    使用 sqruff 替代 SQLFluff

--- sql/import_chips.sql
+++ sql/import_chips.sql
@@ -2,16 +2,16 @@ DROP TABLE IF EXISTS db.chips;
 
 -- 创建表
 CREATE TABLE db.chips (
-    职业 TEXT NOT NULL,
-    芯片类型 TEXT NOT NULL,
-    数量 INTEGER NOT NULL,
-    UNIQUE (职业, 芯片类型)
+    "职业" TEXT NOT NULL,
+    "芯片类型" TEXT NOT NULL,
+    "数量" INTEGER NOT NULL,
+    UNIQUE ("职业", "芯片类型")
 );
 
 -- 导入数据
 INSERT INTO db.chips
 SELECT
-    "职业"::TEXT AS 职业,
-    "芯片类型"::TEXT AS 芯片类型,
-    "数量"::INTEGER AS 数量
+    "职业"::TEXT AS "职业",
+    "芯片类型"::TEXT AS "芯片类型",
+    "数量"::INTEGER AS "数量"
 FROM file;
--- sql/query_short_chips.sql
+++ sql/query_short_chips.sql
@@ -1,7 +1,7 @@
 SELECT *
 FROM file
 WHERE
-    (芯片类型 = '小' AND 数量 < 5)
+    ("芯片类型" = '小' AND "数量" < 5)
     OR
-    (芯片类型 = '大' AND 数量 < 8)
-ORDER BY 数量 DESC;
+    ("芯片类型" = '大' AND "数量" < 8)
+ORDER BY "数量" DESC;
